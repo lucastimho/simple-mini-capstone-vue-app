d95f98d1517373da29b6c8d15dda384a01c34e68
lucastimho <lucas.t.ho.lh@gmail.com>
2021-11-09 14:26:46 -0600

    adds axios request and products with images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,55 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <button v-on:click="createProduct()">Create Product</button>
+    <div v-for="product in products" :key="product.id">
+      <h3>{{ product.name }}</h3>
+      <p>Price: ${{ product.price }}</p>
+      <img v-bind:src="product.image_url" :alt="product.name" />
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 250px;
+}
+</style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
-      data: [
-        { id: 1, name: "Product 1", price: 10 },
-        { id: 2, name: "Product 2", price: 230 },
-      ],
+      message: "Welcome to Lucas's Product Page!",
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products").then((response) => {
+        this.products = response.data;
+        console.log("All products", this.products);
+      });
+    },
+    createProduct: function () {
+      var params = {
+        name: "test",
+        description: "example",
+        image_url: "image.png",
+        price: 1,
+      };
+      axios
+        .post("http://localhost:3000/products", params)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.products.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+  },
 };
 </script>
